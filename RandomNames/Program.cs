76a55cd35b54818281dfeb113a56127cfdc95b49
Jessica Muirhead <jessica.muirhead@glyndwr.ac.uk>
2019-03-26 16:00:00 +0000

    Adjust example code to clarify loop iteration and seed.

--- RandomNames/Program.cs
+++ RandomNames/Program.cs
@@ -14,10 +14,10 @@ namespace RandomNames
             // Note that we are using a seed value -- as long as we specify the same seed, 
             // the same names will be produced every time we run the code.
             // Change this for your program -- any integer is valid
-            Names names = new Names(100);
+            Names names = new Names(50);
 
             // Generate 100 random names
-            for (int i = 0; i < 100; i++)
+            for (int i = 1; i <= 100; i++)
             {
                 String name = names.Generate();
                 Console.WriteLine($"{i}: {name}");
